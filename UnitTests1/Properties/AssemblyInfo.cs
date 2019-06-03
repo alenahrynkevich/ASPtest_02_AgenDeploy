e8216356a9bb5adf25cdea53ff7252be0cde6265
gitHryn2019 <50930810+gitHryn2019@users.noreply.github.com>
2019-06-03 12:57:29 -0700

    Added Unit tests

---
+++ UnitTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14659f1c-a3d7-414c-9fc6-16a5fab09dba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string valueString = "Your string1";
+            Console.WriteLine(valueString);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string valueString = "Your string2";
+            Console.WriteLine(valueString);
+        }
+
+
+    }
+}
